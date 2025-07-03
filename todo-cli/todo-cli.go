b59e1489c5c04fd4cc625d725f2db6646cf98024
izza-sirkast <dawlaizza405@gmail.com>
2025-07-03 14:29:12 +0700

    feat: main program loop

---
+++ todo-cli/todo-cli.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	userReader := bufio.NewReader(os.Stdin)
+	programState := 10
+
+	for programState != 0 {
+		fmt.Println("todo app")
+
+		// get todo data
+		file, err := os.Open("todo.csv")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+
+		reader := csv.NewReader(file)
+		records, err := reader.ReadAll()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		for _, record := range records {
+			fmt.Println(record)
+		}
+
+		fmt.Print("\n\n\n" +
+			"1) Add new todo\n" +
+			"2) Delete a todo\n" +
+			"3) Toggle completion status\n" +
+			"4) Delete all completed todos\n" +
+			"0) Exit program\n" +
+			"What do you want to do [1/2/3/4/0]: ")
+
+		userOptionPick, err := userReader.ReadString('\n')
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		userOptionPickInt, err := strconv.Atoi(strings.TrimSpace(userOptionPick))
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		programState = userOptionPickInt
+
+		switch programState {
+		case 1:
+			fmt.Println("add new todo")
+
+		case 2:
+			fmt.Println("remove a todo")
+
+		case 3:
+			fmt.Println("toggle todo completion")
+
+		case 4:
+			fmt.Println("remove all completed todos")
+
+		case 0:
+			fmt.Println("good bye...")
+
+		}
+	}
+
+	// file, err := os.Create("todo.csv")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// defer file.Close()
+
+	// writer := csv.NewWriter(file)
+	// defer writer.Flush()
+
+	// data := [][]string{
+	// 	{"id", "description", "status"},
+	// 	{"1", "Learn go", "0"},
+	// 	{"2", "Working on projects", "1"},
+	// }
+
+	// if err := writer.WriteAll(data); err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+}
